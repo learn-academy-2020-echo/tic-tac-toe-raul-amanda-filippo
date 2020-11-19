3b1eb8108640ca88c9d7749591d5ebdcb0fddf81
Filippo Venturini <filippo.20u@gmail.com>
2020-11-18 16:47:01 -0800

    end of tuesday

--- src/App.js
+++ src/App.js
@@ -6,9 +6,42 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+			squares: Array(8).fill(null), 
+		// current player
+		// winner
 		};
 	}
+
+// handleGamePlay method
+// take in the index as an argument and passed from the square component
+
+handleGamePlay = (index) => {
+	alert (index)
+}
+
+// check if the clicked square = null (empty) and the winner is null
+// prevent already clicked box to get clicked again
+
+
+// Determine the winning conditions and double check what's in the board right now
+//Winning conditions:
+// Rows:
+// index 0,1,2 
+// index 3,4,5
+// index 6,7,8
+// Columns:
+// index 0,3,6
+// index 1,4,7
+// index 2,5,8
+// Diagonally:
+// 0,4,8
+// 2,4,6 
+
+// check if any of the winning conditions have been met
+// if game won, you can't continue to play the game
+// if no more squares available, show a notice that the game has ended
+
+//  restart game method with button
 
 	render() {
 		const { squares } = this.state;
@@ -18,9 +51,16 @@ class App extends Component {
 				<h1>Tic Tac Toe</h1>
 				<div id='gameboard'>
 					{squares.map((value, index) => {
-						return <Square value={value} key={index} />;
+						return (
+						<Square 
+						value={value} 
+						key={index} 
+						index = {index}
+						handleGamePlay = {this.handleGamePlay}
+						/>)
 					})}
 				</div>
+			{/* button : restart game, uses restart game method */}
 			</React.Fragment>
 		);
 	}
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class Square extends Component {
+	
+//  create the handleClick 
+handleClick = () => {
+	this.props.handleGamePlay(this.props.index)
+}
+
 	render() {
-		const { value } = this.props;
+		// const { value } = this.props;
 
 		return (
 			<React.Fragment>
-				<div id='square'>{value}</div>
+				<div id='square' onClick={this.handleClick} >
+				
+				</div>
 			</React.Fragment>
 		);
 	}
