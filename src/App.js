5beb00540320c154a875b69c1c9f9dc1b43c1f0e
REV004 <rsier004@gmail.com>
2020-11-19 14:23:38 -0800

    finish gameplay method

--- src/App.js
+++ src/App.js
@@ -6,42 +6,48 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			squares: Array(8).fill(null), 
-		// current player
-		// winner
+			squares: Array(9).fill(null),
+			currentPlayer: "x",
+			winner: null,
 		};
 	}
 
-// handleGamePlay method
-// take in the index as an argument and passed from the square component
+	// handleGamePlay method
+	// take in the index as an argument and passed from the square component
 
-handleGamePlay = (index) => {
-	alert (index)
-}
+	handleGamePlay = (index) => {
+		const { squares, currentPlayer, winner } = this.state;
+		// check if the clicked square = null (empty) and the winner is null
+		if (squares[index] === null && winner === null) {
+			squares[index] = currentPlayer;
+			this.setState({
+				squares: squares,
+				currentPlayer: currentPlayer === "x" ? "o" : "x",
+			});
+		}
+	};
 
-// check if the clicked square = null (empty) and the winner is null
-// prevent already clicked box to get clicked again
+	// prevent already clicked box to get clicked again
 
+	// Determine the winning conditions and double check what's in the board right now
+	//Winning conditions:
+	// Rows:
+	// index 0,1,2
+	// index 3,4,5
+	// index 6,7,8
+	// Columns:
+	// index 0,3,6
+	// index 1,4,7
+	// index 2,5,8
+	// Diagonally:
+	// 0,4,8
+	// 2,4,6
 
-// Determine the winning conditions and double check what's in the board right now
-//Winning conditions:
-// Rows:
-// index 0,1,2 
-// index 3,4,5
-// index 6,7,8
-// Columns:
-// index 0,3,6
-// index 1,4,7
-// index 2,5,8
-// Diagonally:
-// 0,4,8
-// 2,4,6 
+	// check if any of the winning conditions have been met
+	// if game won, you can't continue to play the game
+	// if no more squares available, show a notice that the game has ended
 
-// check if any of the winning conditions have been met
-// if game won, you can't continue to play the game
-// if no more squares available, show a notice that the game has ended
-
-//  restart game method with button
+	//  restart game method with button
 
 	render() {
 		const { squares } = this.state;
@@ -52,15 +58,16 @@ handleGamePlay = (index) => {
 				<div id='gameboard'>
 					{squares.map((value, index) => {
 						return (
-						<Square 
-						value={value} 
-						key={index} 
-						index = {index}
-						handleGamePlay = {this.handleGamePlay}
-						/>)
+							<Square
+								value={value}
+								key={index}
+								index={index}
+								handleGamePlay={this.handleGamePlay}
+							/>
+						);
 					})}
 				</div>
-			{/* button : restart game, uses restart game method */}
+				{/* button : restart game, uses restart game method */}
 			</React.Fragment>
 		);
 	}
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-	
-//  create the handleClick 
-handleClick = () => {
-	this.props.handleGamePlay(this.props.index)
-}
+	//  create the handleClick
+	handleClick = () => {
+		this.props.handleGamePlay(this.props.index);
+	};
 
 	render() {
-		// const { value } = this.props;
+		const { value } = this.props;
 
 		return (
 			<React.Fragment>
-				<div id='square' onClick={this.handleClick} >
-				
+				<div id='square' onClick={this.handleClick}>
+					{value}
 				</div>
 			</React.Fragment>
 		);
