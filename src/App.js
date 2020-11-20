a28a2a9c80a1612db53f0019e1cc00743512e706
AKFosterDev <akfoster2015@gmail.com>
2020-11-19 16:47:55 -0800

    work on winning method

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import Square from "./components/Square";
-import "./App.css";
+import React, { Component } from 'react'
+import Square from './components/Square'
+import './App.css'
 
 class App extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 		this.state = {
 			squares: Array(9).fill(null),
-			currentPlayer: "x",
+			currentPlayer: 'x',
 			winner: null,
-		};
+		}
 	}
 
 	// handleGamePlay method
 	// take in the index as an argument and passed from the square component
 
 	handleGamePlay = (index) => {
-		const { squares, currentPlayer, winner } = this.state;
+		const { squares, currentPlayer, winner } = this.state
 		// check if the clicked square = null (empty) and the winner is null
 		if (squares[index] === null && winner === null) {
-			squares[index] = currentPlayer;
+			squares[index] = currentPlayer
 			this.setState({
 				squares: squares,
-				currentPlayer: currentPlayer === "x" ? "o" : "x",
-			});
+				currentPlayer: currentPlayer === 'x' ? 'o' : 'x',
+			})
 		}
-		console.log(squares);
-	};
+		console.log(squares)
+	}
 
 	// prevent already clicked box to get clicked again
 
 	winning = () => {
-		const { squares, currentPlayer } = this.state;
+		const { squares, currentPlayer } = this.state
 
 		// Determine the winning conditions and double check what's in the board right now
 		//Winning conditions:
@@ -44,32 +44,34 @@ class App extends Component {
 			[0, 4, 8],
 			[2, 4, 6],
 			[0, 1, 2],
-		];
-		console.log(winningCombos);
+		]
+		console.log(winningCombos)
 
 		// check if any of the winning conditions have been met
 		// conditional that compares winningCombos to [squares] index
 		winningCombos.map((value) => {
-			console.log(value);
-		});
+			let { a, b, c } = value
+			console.log(a, b, c)
+		})
 
 		// if game won, you can't continue to play the game
 		// if no more squares available, show a notice that the game has ended
 
-		return console.log(winningCombos);
-	};
+		return console.log(winningCombos)
+	}
 	//  restart game method with button
 	restartGame = () => {
 		// set state back to original state
 		this.setState({
 			squares: Array(9).fill(null),
-			currentPlayer: "x",
+			currentPlayer: 'x',
 			winner: null,
-		});
-	};
+		})
+		this.winning()
+	}
 
 	render() {
-		const { squares } = this.state;
+		const { squares } = this.state
 
 		return (
 			<React.Fragment>
@@ -83,13 +85,13 @@ class App extends Component {
 								index={index}
 								handleGamePlay={this.handleGamePlay}
 							/>
-						);
+						)
 					})}
 				</div>
 				{/* button : restart game, uses restart game method */}
 				<button onClick={this.restartGame}> Restart Game </button>
 			</React.Fragment>
-		);
+		)
 	}
 }
-export default App;
+export default App
